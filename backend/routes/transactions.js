67d1b1bad5894f04d36d9fc4120959c79feeb5d4
Seungmin Seok <seungmin3601@gmail.com>
2024-08-29 17:45:18 +1200

    Implemented the get and delete methods to router

--- backend/routes/transactions.js
+++ backend/routes/transactions.js
@@ -1,7 +1,15 @@
-const { addIncome } = require("../controllers/income");
+const {
+  addIncome,
+  getIncomes,
+  deleteIncome,
+} = require("../controllers/income");
 
 const router = require("express").Router();
 
-router.post("/add-income", addIncome);
+router
+  .post("/add-income", addIncome)
+  .get("/get-incomes", getIncomes)
+  .delete("/delete-income/:id", deleteIncome);
+//will pass the id coming from the param
 
 module.exports = router;
