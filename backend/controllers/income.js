d0cd736bc802fe636fc4675131d535377208fbc6
Seungmin Seok <seungmin3601@gmail.com>
2024-08-29 17:26:53 +1200

    Added getIncomes method

--- backend/controllers/income.js
+++ backend/controllers/income.js
@@ -23,6 +23,17 @@ exports.addIncome = async (req, res) => {
     }
     await income.save();
     res.status(200).json({ message: "Income Added" });
-  } catch (error) {}
+  } catch (error) {
+    res.status(500).json({ message: "Server Error" });
+  }
   console.log(income);
+};
+
+exports.getIncomes = async (req, res) => {
+  try {
+    const incomes = await IncomeSchema.find().sort({ createdAt: -1 });
+    res.status(200).json(incomes);
+  } catch (error) {
+    res.status(500).json({ message: "server error" });
+  }
 };
