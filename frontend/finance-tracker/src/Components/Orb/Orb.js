4fabbe6aa84e5f932ad74e6ff69baa8784273bae
Seungmin Seok <seungmin3601@gmail.com>
2024-08-31 15:22:22 +1200

    added animation to the orb

--- frontend/finance-tracker/src/Components/Orb/Orb.js
+++ frontend/finance-tracker/src/Components/Orb/Orb.js
@@ -1,7 +1,19 @@
 import React from "react";
-import { styled } from "styled-components";
+import { keyframes, styled } from "styled-components";
 
 function Orb() {
+  const moveOrb = keyframes`
+    0%{
+        transform: translate(0, 0);
+    }
+    50%{
+        transform: translate(400px, 500px);
+    }
+    100%{
+        transform: translate(0, 0);
+    }
+    `;
+
   const OrbStyled = styled.div`
     width: 70vh;
     height: 70vh;
@@ -11,8 +23,9 @@ function Orb() {
     margin-top: -37vh;
     background: linear-gradient(180deg, #f56692 0%, #f2994a 100%);
     filter: blur(400px);
+    animation: ${moveOrb} 15s alternate linear infinite;
   `;
-
+  //animation will be infinite and 15 sec each
   return <OrbStyled></OrbStyled>;
 }
 
